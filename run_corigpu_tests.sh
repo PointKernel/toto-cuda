4e81f742eaf404597c7888e5c8c317721cf993c9
Yunsong Wang <wangyunsong89@gmail.com>
2019-08-07 12:57:01 -0700

    Update run_corigpu_tests.sh

--- run_corigpu_tests.sh
+++ run_corigpu_tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-MATIRX=128
-while [  $MATIRX -lt 65536 ]; do
-  srun nvprof --print-gpu-summary ./simpleGEMM $MATIRX
-  srun nv-nsight-cu-cli -f --metrics sm__inst_executed_pipe_tensor.avg.pct_of_peak_sustained_active ./simpleGEMM $MATIRX
-  let MATIRX=MATIRX*2
+MATRIX=128
+while [  $MATRIX -lt 65536 ]; do
+  srun nvprof --print-gpu-summary ./simpleGEMM $MATRIX
+  srun nv-nsight-cu-cli -f --metrics sm__inst_executed_pipe_tensor.avg.pct_of_peak_sustained_active ./simpleGEMM $MATRIX
+  let MATRIX=MATRIX*2
 done
