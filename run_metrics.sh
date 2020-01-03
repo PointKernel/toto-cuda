d86f1b7ff377e8ae7953805280d13dd38535a449
Yunsong Wang <yunsongwang@lbl.gov>
2020-01-02 16:50:52 -0800

    Add measurement with new metrics

--- run_metrics.sh
+++ run_metrics.sh
@@ -1,33 +1,20 @@
 #!/bin/bash
 
-MATIRX=4096
-while [  $MATIRX -lt 8192 ]; do
-  srun nvprof --metrics flop_count_hp,flop_count_sp	./simpleGEMM $MATIRX
-  srun nv-nsight-cu-cli -f --metrics \
-  sm__inst_executed_pipe_tensor.avg.pct_of_peak_sustained_active,\
+srun nv-nsight-cu-cli -f --metrics \
+sm__inst_executed_pipe_tensor.avg.pct_of_peak_sustained_active,\
+smsp__sass_thread_inst_executed_op_dadd_pred_on.sum,\
+smsp__sass_thread_inst_executed_op_dmul_pred_on.sum,\
+smsp__sass_thread_inst_executed_op_dfma_pred_on.sum,\
 smsp__sass_thread_inst_executed_op_fadd_pred_on.sum,\
 smsp__sass_thread_inst_executed_op_fmul_pred_on.sum,\
 smsp__sass_thread_inst_executed_op_ffma_pred_on.sum,\
 smsp__sass_thread_inst_executed_op_hadd_pred_on.sum,\
 smsp__sass_thread_inst_executed_op_hmul_pred_on.sum,\
-smsp__sass_thread_inst_executed_op_hfma_pred_on.sum\
-  ./simpleGEMM $MATIRX
-
-  srun nvprof --metrics gld_transactions,\
-gst_transactions,\
-atomic_transactions,\
-local_load_transactions,\
-local_store_transactions,\
-shared_load_transactions,\
-shared_load_transactions,\
-l2_read_transactions,\
-l2_write_transactions,\
-dram_read_transactions,\
-dram_write_transactions,\
-system_read_transactions,\
-system_write_transactions\
-  ./simpleGEMM $MATIRX
-  srun nv-nsight-cu-cli -f --metrics \
+smsp__sass_thread_inst_executed_op_hfma_pred_on.sum,\
+smsp__cycles_elapsed.sum,\
+smsp__cycles_elapsed.sum.per_second,\
+smsp__pipe_tensor_op_hmma_cycles_active.sum,\
+smsp__pipe_tensor_op_hmma_cycles_active.sum.per_second,\
 l1tex__t_sectors_pipe_lsu_mem_global_op_ld.sum,\
 l1tex__t_sectors_pipe_lsu_mem_global_op_st.sum,\
 l1tex__t_set_accesses_pipe_lsu_mem_global_op_atom.sum,\
@@ -44,6 +31,4 @@ dram__sectors_read.sum,\
 dram__sectors_write.sum,\
 lts__t_sectors_aperture_sysmem_op_read.sum,\
 lts__t_sectors_aperture_sysmem_op_write.sum\
-  ./simpleGEMM $MATIRX
-  let MATIRX=MATIRX*2
-done
+  ./simpleGEMM
