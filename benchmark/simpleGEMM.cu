4a16c66fd0a31ba372217586dc8fc8d054d5b714
Yunsong Wang <yunsongwang@lbl.gov>
2020-03-04 12:48:18 -0800

    Minor thing

--- benchmark/simpleGEMM.cu
+++ benchmark/simpleGEMM.cu
@@ -22,7 +22,7 @@ void init(half *A, half *B, float *C, size_t m, size_t n, size_t k) {
       B[i * n + j] = __float2half(static_cast<float>(rand() % 100));
   for (size_t i = 0; i < m; i++)
     for (size_t j = 0; j < n; j++)
-      A[i * n + j] = static_cast<float>(rand() % 100);
+      C[i * n + j] = static_cast<float>(rand() % 100);
 }
 
 int main(int argc, char *argv[]) {
