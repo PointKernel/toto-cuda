91a6610c9def0007cb5ec6390af0c37a55b013b1
Yunsong Wang <yunsongwang@lbl.gov>
2020-07-16 15:27:31 -0700

    Unittest of a mysterious use case of shfl up

--- test/simpleShuffle.cu
+++ test/simpleShuffle.cu
@@ -3,25 +3,22 @@
 
 #define FULL_MASK 0xffffffff
 
-__global__ void shfl_up_kernel(uint32_t *array_d) {
-  uint32_t x = 1;
-  uint32_t laneId = 0;
-  uint32_t i = 0;
-  for (int32_t offset = 1; offset < 32; offset <<= 1) {
-    auto y = __shfl_up_sync(FULL_MASK, x, offset);
-    if (blockIdx.x == 0)
-      array_d[i * 32 + threadIdx.x] = y;
-    __syncthreads();
-    if (laneId >= offset)
-      x += y;
-    i++;
-  }
-  if (blockIdx.x == 0)
+__global__ void shfl_up_kernel(int32_t *array_d) {
+  if (threadIdx.x < 256) {
+    auto x = array_d[threadIdx.x];
+    auto laneId = threadIdx.x & 0x1f;
+#pragma unroll
+    for (int offset = 1; offset < 32; offset <<= 1) {
+      auto y = __shfl_up_sync(FULL_MASK, x, offset);
+      if (laneId >= offset)
+        x += y;
+    }
     array_d[threadIdx.x] = x;
+  }
 }
 
 __global__ void shfl_down_kernel(int *array_d) {
-  int val = 2;
+  int val = 1;
   for (int offset = 16; offset > 0; offset /= 2)
     val += __shfl_down_sync(FULL_MASK, val, offset);
   if (threadIdx.x == 0)
@@ -37,23 +34,29 @@ int main() {
 
   shfl_down_kernel<<<32, 32>>>(array_d);
   cudaMemcpy(reinterpret_cast<void *>(array_h),
-             reinterpret_cast<void *>(array_d), 32, cudaMemcpyDeviceToHost);
+             reinterpret_cast<void *>(array_d), 32 * sizeof(int), cudaMemcpyDeviceToHost);
   cudaDeviceSynchronize();
 
   std::cout << array_h[0] << "\n\n";
 
-  uint32_t *a_h, *a_d;
+  int32_t *a_h, *a_d;
 
-  a_h = static_cast<uint32_t *>(malloc(sizeof(uint32_t) * 32 * 6));
+  const int N = 512;
+  a_h = static_cast<int32_t *>(malloc(sizeof(int32_t) * N));
+  for (int i = 0; i < N; i++)
+    a_h[i] = i;
+
   checkCudaErrors(
-      cudaMalloc(reinterpret_cast<void **>(&a_d), sizeof(uint32_t) * 32 * 6));
+      cudaMalloc(reinterpret_cast<void **>(&a_d), sizeof(int32_t) * N));
 
-  shfl_up_kernel<<<32, 32>>>(a_d);
+  cudaMemcpy(reinterpret_cast<void *>(a_d), reinterpret_cast<void *>(a_h),
+             sizeof(int32_t) * N, cudaMemcpyHostToDevice);
+  shfl_up_kernel<<<1, 512>>>(a_d);
   cudaMemcpy(reinterpret_cast<void *>(a_h), reinterpret_cast<void *>(a_d),
-             32 * 6, cudaMemcpyDeviceToHost);
+             sizeof(int32_t) * N, cudaMemcpyDeviceToHost);
   cudaDeviceSynchronize();
 
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < N / 32; i++) {
     for (int j = 0; j < 32; j++) {
       std::cout << a_h[i * 32 + j] << " ";
     }
