c95205b64a08a44e959ee033c68d1620e6eaf5fd
Yunsong Wang <yunsongw@nvidia.com>
2022-02-04 15:03:21 -0500

    Add stream test

---
+++ test/simpleStream.cu
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  cudaStream_t stream;
+
+  cudaStreamCreate(&stream);
+
+  std::cout << cudaStreamDestroy(stream) << "\n";
+  std::cout << cudaStreamDestroy(stream) << "\n";
+  std::cout << cudaStreamDestroy(stream) << "\n";
+
+  return 0;
+}
