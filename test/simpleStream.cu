b89f04e5b5c323f3463733e19c01ce33a49d9722
Yunsong Wang <yunsongw@nvidia.com>
2022-02-18 16:57:33 -0500

    Add simple stream test

--- test/simpleStream.cu
+++ test/simpleStream.cu
@@ -1,16 +1,18 @@
 #include <iostream>
 
+#include <assert.h>
+
 int main()
 {
   cudaStream_t stream;
 
   cudaStreamCreate(&stream);
 
-  std::cout << cudaStreamDestroy(stream) << "\n";
-  std::cout << cudaStreamDestroy(stream) << "\n";
-  std::cout << cudaStreamDestroy(stream) << "\n";
+  assert(cudaStreamDestroy(stream) == 0 /* destroyed without issues*/);
+  assert(cudaStreamDestroy(stream) == 709 /* cudaErrorContextIsDestroyed */);
 
-  std::cout << cudaStreamDestroy(cudaStreamDefault) << "\n";
+  // Default stream is non-owning thus should not be destroyed by users
+  assert(cudaStreamDestroy(0 /*default stream*/) == 400 /* cudaErrorInvalidResourceHandle */);
 
   return 0;
 }
